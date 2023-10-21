374b4a09bc808ca90f0cc3e258c00c2d19934094
lam <72236208+Lnguyen19@users.noreply.github.com>
2023-10-20 17:08:23 -0700

    hdyd

--- src/self_profile.jsx
+++ src/self_profile.jsx
@@ -85,8 +85,9 @@ axios.post(`https://mysocial-1473059facea.herokuapp.com/addProfilePicture`,formD
 })}
 const uploadBackgroundPic = ()=>{
 const formData = new FormData();
+formData.append('username',username.username);
 formData.append('background',background);
-axios.post(`https://mysocial-1473059facea.herokuapp.com/addBackgroundPicture/${profile.username}`,formData,{withCredentials:true}).then(response=>{
+axios.post(`https://mysocial-1473059facea.herokuapp.com/addBackgroundPicture`,formData,{withCredentials:true}).then(response=>{
 setBackground_picture(`https://res.cloudinary.com/dmyyrftce/image/upload/${response.data.background}`)
 console.log('sent background successfully');
 
