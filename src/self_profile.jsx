9bcdd9ca2f31566b8ff6286c094dc4215f2eefe0
lam <72236208+Lnguyen19@users.noreply.github.com>
2023-10-20 17:36:13 -0700

    kk

--- src/self_profile.jsx
+++ src/self_profile.jsx
@@ -88,7 +88,7 @@ const uploadBackgroundPic = () => {
   formData.append('username', username.username);
   formData.append('background', background);
 
-  axios.post(`https://mysocial-1473059facea.herokuapp.com/addBackgroundPicture`, formData, { withCredentials: true })
+  axios.post("https://mysocial-1473059facea.herokuapp.com/addBackgroundPicture", formData, { withCredentials: true })
     .then(response => {
       setBackground_picture(`https://res.cloudinary.com/dmyyrftce/image/upload/${response.data.background}`);
       console.log('sent background successfully');
